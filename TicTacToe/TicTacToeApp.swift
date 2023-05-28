1e5af19f1f7dfd8eee845f4dd7d41a9127f64bf3
Sachin Panigrahi <sachin.panigrahi@tcs.com>
2023-05-28 14:32:42 +0530

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Sachin Panigrahi on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
